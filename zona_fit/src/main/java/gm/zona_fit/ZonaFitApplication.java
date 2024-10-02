a83a46c6abef236624107de0c9a959d37b58d840
Esteban Necuse <estebannecuse@gmail.com>
2024-10-02 10:43:34 -0300

    config y declaracion de clases ok

--- zona_fit/src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ zona_fit/src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -1,14 +1,30 @@
 package gm.zona_fit;
 
+import gm.zona_fit.servicio.IClienteServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZonaFitApplication {
+public class ZonaFitApplication implements CommandLineRunner {
+
+	@Autowired
+	private IClienteServicio clienteServicio;
+
+	private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class); // esto es para poder mandar info a la consola y que clase esta mandando la info , es para mayor detalle
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(ZonaFitApplication.class, args);
+		logger.info("Iniciando la aplicacion amigoo");
+		SpringApplication.run(ZonaFitApplication.class, args); // se ejecuta la " fabrica de objetos spring empieza a crear todos los objetos para nosotros poder usar la app
+		logger.info("Aplicacion finalizada amigoo");
 	}
 
+	// cuando se levanta la app y tenemos los objetos disponibles, ahora si podemos ejecutar la app y usar los objetos
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Aplicacion Zona Fit ***");
+	}
 }
